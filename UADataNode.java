9f55af8fb96433acfc78b5f29f398f89c9082829
kylek44 <kylek44@outlook.com>
2019-12-11 03:52:18 -0600

    Update UADataNode.java

--- UADataNode.java
+++ UADataNode.java
@@ -1,6 +1,32 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class UADataNode {
+    private static final int PORT = 32001;
+    private ServerSocket server;
 
+    public UADataNode() {
+        try {
+            server = new ServerSocket(PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void start() {
+        while (true) {
+            try {
+                Socket socket = server.accept();
+                new UADataConnection(socket).run();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+        UADataNode node = new UADataNode();
+        node.start();
+    }
 }
